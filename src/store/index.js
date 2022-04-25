cb62b584b09058b5d0b63878ef203b260771f731
NeginSal <neginsaljooghi@gmail.com>
2022-04-25 16:38:46 +0430

    first commit

---
+++ src/store/index.js
@@ -0,0 +1,19 @@
+import { createStore } from 'vuex'
+
+export default createStore({
+  state: {
+    products: [
+      { name: "Banana Skin", price: 20 },
+      { name: "Shiny Star", price: 40 },
+      { name: "Green Shells", price: 60 },
+      { name: "Red Shells", price: 80 }, 
+    ],
+    count: [],
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
